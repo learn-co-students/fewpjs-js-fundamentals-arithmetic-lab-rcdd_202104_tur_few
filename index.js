acf3aa5ecf8ac4d9a1a74faa027cebb3ad1daed2
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-05-02 15:20:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+
+let multiply = num1 * num2
+console.log(multiply)
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+let num3 = 99
+let num4 = 5
+let mod = num3 % num4
+console.log(mod)
+
+
+let max = Math.max(20, 2, 3, 4, 5)
+console.log(max)
+
+
