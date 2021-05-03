ab91121effb7520a3304d42742bb0e5a966265e8
abduallahB <abduallah.imu2@gmail.com>
2021-05-04 02:07:26 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+//1
+let num1 = 9 ,num2 = 7 , num3 = 19, num4 = 4; 
+let multiply = (num1 * num2) - 1;
+//2
+let random = Math.ceil(Math.random() * 10) ;
+//console.log(random)
+
+//3
+let mod = Math.floor((num3 / num4) % 9) //+ 1 ;
+
+//console.log(mod)
+
+
+//4
+let max = Math.max(20,4,5,6,7,)
+//console.log(max)
