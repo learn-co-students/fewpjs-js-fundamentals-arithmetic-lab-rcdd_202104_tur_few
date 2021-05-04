0c9c94d056feb6620cd77a870e30942bac3d96dc
DKay-code <derya.kaygisiz@outlook.com>
2021-05-04 19:55:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 1;
+const num2 = 62;
+const num3 = 22;
+const num4 = 6;
+
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = 22%6;
+
+const max = Math.max(0, 20, 3, 13);
